b1653ba537c4b6f1c4eec161f10238040bfcd2e3
cherylchao6 <nmpyt21@gmail.com>
2024-07-03 18:06:23 -0400

    chane view

--- server.js
+++ server.js
@@ -27,8 +27,9 @@ legoData
 const express = require("express");
 const app = express();
 const port = 3000;
-app.set("view engine", "ejs");
+app.set("views", __dirname + "/../views");
 app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("home");
